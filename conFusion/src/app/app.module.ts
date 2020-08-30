0ad09d00ca4119320884f913b213dc015c6670fa
PrakrutiChandak <prakrutichandak@gmail.com>
2020-08-31 00:35:58 +0530

    menu

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -8,10 +8,12 @@ import 'hammerjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
