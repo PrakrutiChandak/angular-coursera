258c30bc091863f6aaf2be764bbab9bba53aa457
PrakrutiChandak <prakrutichandak@gmail.com>
2020-08-31 19:14:28 +0530

    week 1 completed

--- conFusion/src/app/shared/dish.ts
+++ conFusion/src/app/shared/dish.ts
@@ -3,7 +3,7 @@ export class Dish {
     name: string;
     image: string;
     category:string;
-    featired: boolean;
+    featured: boolean;
     label: string;
     price: string;
     description: string;
